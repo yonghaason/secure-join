f8f15df0676788231ac4399945c9cf32249a1f71
Peter Rindal <perindal@visa.com>
2023-08-16 13:08:47 -0700

    comments and todos

--- secure-join/Aggregate/Average.cpp
+++ secure-join/Aggregate/Average.cpp
@@ -1,7 +1,7 @@
 #include "Average.h"
 // #include "secure-join/Join/OmJoin.h"
 
-namespace secJoin{
+namespace secJoin {
 
     // concatinate all the columns in `average` that are part of the table.
     // Then append 1's the end for the count
@@ -19,7 +19,7 @@ namespace secJoin{
         std::vector<BinMatrix*> avg;
 
         offsets.clear();
-        offsets.reserve(m+1);
+        offsets.reserve(m + 1);
         for (u64 i = 0; i < m; ++i)
         {
             if (&groupByCol.mTable == &avgCol[i].mTable)
@@ -35,24 +35,24 @@ namespace secJoin{
                 std::string temp("Average table is not same as groupby table\n");
                 throw std::runtime_error(temp + LOCATION);
             }
-            
+
         }
 
         // Adding a Columns of 1's for calculating average
         BinMatrix ones(n0, sizeof(oc::u64) * 8);
 
         // Adding 1's in only party column
-        if(ole.mRole == OleGenerator::Role::Receiver)
+        if (ole.mRole == OleGenerator::Role::Receiver)
         {
-            for(oc::u64 i = 0; i < n0; i++)
-                ones(i,0) = 1;
+            for (oc::u64 i = 0; i < n0; i++)
+                ones(i, 0) = 1;
         }
-        
-        
+
+
         offsets.emplace_back(OmJoin::Offset{ rowSize * 8, sizeof(oc::u64) * 8, "count*" });
         avg.emplace_back(&ones);
 
-        ret.resize(n0, (rowSize +sizeof(oc::u64) ) * 8);
+        ret.resize(n0, (rowSize + sizeof(oc::u64)) * 8);
         OmJoin::concatColumns(ret, avg);
 
     }
@@ -107,7 +107,7 @@ namespace secJoin{
     }
 
 
-   macoro::task<> print(
+    macoro::task<> print(
         const BinMatrix& data,
         coproto::Socket& sock,
         int role,
@@ -161,11 +161,11 @@ namespace secJoin{
             addCir = AggTree::Operator{},
             aggTree = AggTree{},
             perm = ComposedPerm{}
-            );
+        );
 
         keys = groupByCol.mCol.mData;
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(keys, sock, (int)ole.mRole, "preSort-keys"));
 
 
@@ -175,7 +175,7 @@ namespace secJoin{
         MC_AWAIT(sort.genPerm(keys, sPerm, ole, sock));
         concatColumns(groupByCol, avgCol, data, offsets, ole);
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(data, sock, (int)ole.mRole, "preSort-data", offsets));
 
         temp.resize(data.numEntries(), data.bytesPerEntry() * 8);
@@ -184,20 +184,20 @@ namespace secJoin{
         MC_AWAIT(sPerm.apply(data, temp, prng, sock, ole, true));
         std::swap(data, temp);
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(data, sock, (int)ole.mRole, "sort-data", offsets));
 
         temp.resize(keys.numEntries(), keys.bytesPerEntry() * 8);
         MC_AWAIT(sPerm.apply(keys, temp, prng, sock, ole, true));
         std::swap(keys, temp);
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(keys, sock, (int)ole.mRole, "sort-keys"));
-        
+
         // compare adjacent keys. controlBits[i] = 1 if k[i]==k[i-1].
         MC_AWAIT(getControlBits(keys, sock, controlBits, ole));
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(controlBits, sock, (int)ole.mRole, "controlbits"));
 
 
@@ -207,13 +207,16 @@ namespace secJoin{
         MC_AWAIT(aggTree.apply(data, controlBits, addCir, AggTreeType::Suffix, sock, ole, temp));
         std::swap(data, temp);
 
-        if(mInsecurePrint)
+        if (mInsecurePrint)
             MC_AWAIT(print(data, sock, (int)ole.mRole, "agg-data", offsets));
 
-
-        if(!mInsecureMockSubroutines)
+        // TODO, i dont think this is the right variable name.
+        // Should this still be here? is there a better way, if(debug) copy state to members;
+        if (!mInsecureMockSubroutines)
         {
-            perm.init( data.numEntries(), (int)ole.mRole, prng);
+            perm.init(data.numEntries(), (int)ole.mRole, prng);
+
+            //TODO: add perm prococessing once peter merges the new code.
 
             temp.resize(data.numEntries(), data.bytesPerEntry() * 8);
             MC_AWAIT(perm.apply<u8>(data.mData, temp.mData, sock, ole, true));
@@ -227,10 +230,10 @@ namespace secJoin{
             MC_AWAIT(perm.apply<u8>(controlBits.mData, temp.mData, sock, ole, true));
             std::swap(controlBits, temp);
         }
-    
+
 
         // Revealing control bits
-        if(ole.mRole == OleGenerator::Role::Receiver)
+        if (ole.mRole == OleGenerator::Role::Receiver)
         {
             temp.resize(controlBits.numEntries(), controlBits.bitsPerEntry());
             MC_AWAIT(sock.recv(temp.mData));
@@ -246,14 +249,15 @@ namespace secJoin{
             std::swap(controlBits, temp);
         }
 
- 
+
         getOutput(out, avgCol, groupByCol, keys, data, controlBits, offsets);
 
+        // TODO: remove, no mIsActive =  all active.
         // Manually setting the isActive flag
         out.mIsActive.resize(out.mColumns[0].mData.numEntries());
-        if(ole.mRole == OleGenerator::Role::Receiver)
+        if (ole.mRole == OleGenerator::Role::Receiver)
         {
-            for(u64 i =0; i < out.mIsActive.size(); i++)
+            for (u64 i = 0; i < out.mIsActive.size(); i++)
                 out.mIsActive[i] = 1;
         }
 
@@ -275,10 +279,10 @@ namespace secJoin{
         // Counting the total number of rows in the 
         u64 nOutRows = 0;
         assert(data.numEntries() == keys.numEntries());
-        for(u64 i =0; i<data.numEntries(); i++)
+        for (u64 i = 0; i < data.numEntries(); i++)
         {
             // Only where Control Bit is zero, we have our desire value
-            if(controlBits.mData(i,0) == 0)
+            if (controlBits.mData(i, 0) == 0)
                 nOutRows++;
         }
 
@@ -293,80 +297,80 @@ namespace secJoin{
         out.mColumns[0].mData.resize(nOutRows, groupByCol.mCol.mBitCount);
 
         // Adding the average cols
-        for(u64 i=0; i < nAvg; i++)
+        for (u64 i = 0; i < nAvg; i++)
         {
-            out.mColumns[i+1].mName = avgCol[i].mCol.mName;
-            out.mColumns[i+1].mBitCount = avgCol[i].mCol.getByteCount() * 8;
-            out.mColumns[i+1].mType = avgCol[i].mCol.mType;
-            out.mColumns[i+1].mData.resize(nOutRows, avgCol[i].mCol.mBitCount);
+            out.mColumns[i + 1].mName = avgCol[i].mCol.mName;
+            out.mColumns[i + 1].mBitCount = avgCol[i].mCol.getByteCount() * 8;
+            out.mColumns[i + 1].mType = avgCol[i].mCol.mType;
+            out.mColumns[i + 1].mData.resize(nOutRows, avgCol[i].mCol.mBitCount);
         }
 
         // Adding the count col
-        out.mColumns[nAvg+1].mName = "Count";
-        out.mColumns[nAvg+1].mBitCount = sizeof(oc::u64) * 8;
-        out.mColumns[nAvg+1].mType = TypeID::IntID;
-        out.mColumns[nAvg+1].mData.resize(nOutRows, sizeof(oc::u64) * 8);
+        out.mColumns[nAvg + 1].mName = "Count";
+        out.mColumns[nAvg + 1].mBitCount = sizeof(oc::u64) * 8;
+        out.mColumns[nAvg + 1].mType = TypeID::IntID;
+        out.mColumns[nAvg + 1].mData.resize(nOutRows, sizeof(oc::u64) * 8);
 
 
         assert(data.numEntries() == keys.numEntries());
         u64 curOutRow = 0;
-        for(u64 i =0; i<data.numEntries(); i++)
+        for (u64 i = 0; i < data.numEntries(); i++)
         {
             assert(curOutRow <= nOutRows);
-            if(controlBits.mData(i,0) == 0)
+            if (controlBits.mData(i, 0) == 0)
             {
                 // Storing the Group By Column
                 assert(out.mColumns[0].mData.cols() == keys.cols());
                 memcpy(out.mColumns[0].mData.data(curOutRow), keys.data(i), keys.cols());
-                
+
                 // Copying the average columns
-                for(u64 j=0; j<offsets.size(); j++)
+                for (u64 j = 0; j < offsets.size(); j++)
                 {
-                    memcpy(out.mColumns[j+1].mData.data(curOutRow),
-                           &data(i, offsets[j].mStart/8),
-                           out.mColumns[j+1].getByteCount());
+                    memcpy(out.mColumns[j + 1].mData.data(curOutRow),
+                        &data(i, offsets[j].mStart / 8),
+                        out.mColumns[j + 1].getByteCount());
 
                 }
                 curOutRow++;
             }
         }
-        
+
     }
 
-    AggTree::Operator Average::getAddCircuit(std::vector<OmJoin::Offset>& offsets, 
-            oc::BetaLibrary::Optimized op)
+    AggTree::Operator Average::getAddCircuit(std::vector<OmJoin::Offset>& offsets,
+        oc::BetaLibrary::Optimized op)
     {
         return [&, op](
             oc::BetaCircuit& cd,
             const oc::BetaBundle& left,
             const oc::BetaBundle& right,
             oc::BetaBundle& out)
-        {
-
-            for(u64 i=0; i < offsets.size(); i++)
             {
-                auto size = offsets[i].mSize;
-                auto beginIndex = offsets[i].mStart;
-                auto endIndex = offsets[i].mStart + size;
-                BetaBundle a, b ,c;
 
-                a.mWires.reserve(size);
-                b.mWires.reserve(size);
-                c.mWires.reserve(size);
-                for(u64 j=beginIndex; j<endIndex; j++)
+                for (u64 i = 0; i < offsets.size(); i++)
                 {
-                    a.mWires.emplace_back(left[j]);
-                    b.mWires.emplace_back(right[j]);
-                    c.mWires.emplace_back(out[j]);
+                    auto size = offsets[i].mSize;
+                    auto beginIndex = offsets[i].mStart;
+                    auto endIndex = offsets[i].mStart + size;
+                    BetaBundle a, b, c;
+
+                    a.mWires.reserve(size);
+                    b.mWires.reserve(size);
+                    c.mWires.reserve(size);
+                    for (u64 j = beginIndex; j < endIndex; j++)
+                    {
+                        a.mWires.emplace_back(left[j]);
+                        b.mWires.emplace_back(right[j]);
+                        c.mWires.emplace_back(out[j]);
+                    }
+
+                    BetaBundle t(op == oc::BetaLibrary::Optimized::Size ? 4 : size * 2);
+                    cd.addTempWireBundle(t);
+                    osuCrypto::BetaLibrary::add_build(cd, a, b, c, t, oc::BetaLibrary::IntType::TwosComplement, op);
+
                 }
 
-                BetaBundle t(op == oc::BetaLibrary::Optimized::Size ? 4 : size * 2);
-                cd.addTempWireBundle(t);
-                osuCrypto::BetaLibrary::add_build(cd, a, b, c, t, oc::BetaLibrary::IntType::TwosComplement, op);
-
-            }
-
-        };
+            };
     }
 
     macoro::task<> Average::getControlBits(
@@ -387,7 +391,7 @@ namespace secJoin{
         keyByteSize = keys.bytesPerEntry();
         keyBitCount = keys.bitsPerEntry();
         cir = OmJoin::getControlBitsCircuit(keyBitCount);
-        sKeys.resize(n+1, keyBitCount);
+        sKeys.resize(n + 1, keyBitCount);
         memcpy(sKeys.data(1), keys.data(0), n * keyByteSize);
         // for (u64 i = 0; i < n; ++i)
         // {
